54cd2703e5a8134b4ac973b5c8ed99563a2c640c
Chris Hanna <christopher.g.hanna@gmail.com>
2023-01-31 18:34:03 -0500

    fix

--- m3-crud-apis/Controllers/M3Controller.cs
+++ m3-crud-apis/Controllers/M3Controller.cs
@@ -24,7 +24,7 @@ namespace m3_crud_apis.Controllers
             long unixTime_start = ((DateTimeOffset)start).ToUnixTimeSeconds();
             long unixTime_end = ((DateTimeOffset)end).ToUnixTimeSeconds();
 
-            var url = "http://10.64.13.7:7201/api/v1/query_range?query=third_avenue&start=" + unixTime_start + "&end=" + unixTime_end + "&step=60s"; 
+            var url = "http://10.64.13.7:7201/api/v1/query_range?query=third_avenue&start=" + unixTime_start + "&end=" + unixTime_end + "&step=60s";
             var client = new RestClient(url);
             var request = new RestRequest(url, Method.Get);
             RestResponse response = client.Execute(request);
@@ -33,34 +33,6 @@ namespace m3_crud_apis.Controllers
             return Output;
         }
 
-        [HttpGet(Name = "GetData2")]
-        public string GetData2()
-        {
-            DateTime start = DateTime.Now.AddDays(-1);
-            DateTime end = DateTime.Now;
-
-            long unixTime_start = ((DateTimeOffset)start).ToUnixTimeSeconds();
-            long unixTime_end = ((DateTimeOffset)end).ToUnixTimeSeconds();
-
-            var url = "http://10.0.0.16:7201/api/v1/query_range?query=third_avenue&start=" + unixTime_start + "&end=" + unixTime_end + "&step=60s";
-            var client = new RestClient(url);
-            var request = new RestRequest(url, Method.Get);
-            RestResponse response = client.Execute(request);
-            string Output = response.Content.ToString();
-
-            return Output;
-        }
-
-        [HttpGet(Name = "GetTest")]
-        public string GetTest()
-        {
-            var url = "https://reqres.in/api/users?page=2";
-            var client = new RestClient(url);
-            var request = new RestRequest(url, Method.Get);
-            RestResponse response = client.Execute(request);
-            string Output = response.Content.ToString();
-
-            return Output;
-        }
+       
     }
 }
