789362539553200991d7a3175034cbe70bea6865
Chris Hanna <christopher.g.hanna@gmail.com>
2023-01-31 19:23:59 -0500

    test v4

--- m3-crud-apis/Controllers/M3Controller.cs
+++ m3-crud-apis/Controllers/M3Controller.cs
@@ -31,11 +31,30 @@ namespace m3_crud_apis.Controllers
             RestResponse response = client.Execute(request);
             string Output = response.Content.ToString();
 
-            Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(Output);            
+            Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(Output); 
+            
+            Dictionary<string, string> yay = new Dictionary<string, string>();
 
-            return myDeserializedClass.data.result.ToString();
+            foreach(var i in myDeserializedClass.data.result)
+            {
+                foreach (var o in i.values) {
+
+                   string date = o[0].ToString();
+                   string value = o[1].ToString();
+
+                    yay.Add(date, value);
+                }                
+            }
+
+            return MyDictionaryToJson(yay);
         }
 
-       
+        string MyDictionaryToJson(Dictionary<string, string> dict)
+        {
+            var entries = dict.Select(d =>
+                string.Format("\"{0}\": [{1}]", d.Key, string.Join(",", d.Value)));
+            return "{" + string.Join(",", entries) + "}";
+        }
+
     }
 }
